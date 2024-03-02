e6529792b38e09bd71465bb9f724986be275dc91
naimaabd <143902711+naimaabd@users.noreply.github.com>
2024-03-02 18:03:33 +0200

    My first EC2 using terraform

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "ami" {
+  description = "the ami of the EC2 instance"
+  type        = string
+
+}
+variable "instance_type" {
+  description = "the type of the EC2 instance"
+  type        = string
+
+}
